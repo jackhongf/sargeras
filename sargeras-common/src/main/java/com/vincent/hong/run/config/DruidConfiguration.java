7fe60c45a3d4f8988cccccc95c32ae61abe1b428
jackhongf <jackhongf@163.com>
2019-07-21 17:09:23 +0800

    注释

--- sargeras-common/src/main/java/com/vincent/hong/run/config/DruidConfiguration.java
+++ sargeras-common/src/main/java/com/vincent/hong/run/config/DruidConfiguration.java
@@ -44,10 +44,14 @@ public class DruidConfiguration {
         servletRegistrationBean.setServlet(new StatViewServlet());
         servletRegistrationBean.addUrlMappings("/druid/*");
         Map<String, String> initParameters = new HashMap<String, String>();
-        initParameters.put("loginUsername", "admin");// 用户名
-        initParameters.put("loginPassword", "admin");// 密码
-        initParameters.put("resetEnable", "false");// 禁用HTML页面上的“Reset All”功能
-        initParameters.put("allow", ""); // IP白名单 (没有配置或者为空，则允许所有访问)
+        // 用户名
+        initParameters.put("loginUsername", "admin");
+        // 密码
+        initParameters.put("loginPassword", "admin");
+        // 禁用HTML页面上的“Reset All”功能
+        initParameters.put("resetEnable", "false");
+        // IP白名单 (没有配置或者为空，则允许所有访问)
+        initParameters.put("allow", "");
         // initParameters.put("deny", "192.168.20.38");// IP黑名单
         // (存在共同时，deny优先于allow)
         servletRegistrationBean.setInitParameters(initParameters);
